05807fab582b300e669822524a7c8c6ec9da2830
HrVasilev <hristo.vasilev@volley-labs.com>
2023-06-16 14:39:48 +0300

    Add controller.

---
+++ FileUploadAPI/Controllers/FileUploadController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FileUploadAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FileUploadController : ControllerBase
+	{
+		private readonly ILogger<FileUploadController> _logger;
+
+		public FileUploadController(ILogger<FileUploadController> logger)
+		{
+			_logger = logger;
+		}
+	}
+}
